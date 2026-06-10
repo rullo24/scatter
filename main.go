92db30d723f23dd7fe31d7b9ddf3100c365f304e
kali <rullo@vektislabs.com>
2026-06-10 12:50:57 +0800

    [fix] - resolved issue where equals sign rune was parsed w/ value of curr arg

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,23 +18,32 @@ type Arguments struct {
 func main() {
 
 	// capture arguments and store into Args struct
+	arguments := Arguments{}
 	for _, arg := range os.Args {
 		// find index of equals sign in current arg (if avail)
 		curr_equals_index := strings.IndexRune(arg, '=')
 		if curr_equals_index == -1 { // failure -> couldn't find '=' rune in curr arg
 			continue
+		} else if len(arg) == (curr_equals_index + 1) {
+			continue // failure -> no text after equals
 		}
+		curr_val_after_equals := arg[(curr_equals_index + 1):]
 
 		// if equals sign avail, switch text before equals sign (to find which argument)
 		switch arg[0:curr_equals_index] {
 		case "--src":
-			fmt.Println("Found src")
+			arguments.src_loc = curr_val_after_equals // validity not checked yet
 
 		case "--dst":
-			fmt.Println("Found dst")
+			arguments.dst_loc = curr_val_after_equals // validity not checked yet
 
 		case "--workers":
-			fmt.Println("Found workers")
+			num_workers, worker_parse_err := strconv.Atoi(curr_val_after_equals)
+			if worker_parse_err != nil {
+				log.Printf("ERROR: Could not parse worker argument (%s)", curr_val_after_equals)
+				os.Exit(-1)
+			}
+			arguments.num_workers = num_workers
 
 		case "--block_size":
 			fmt.Println("Found block_size")
